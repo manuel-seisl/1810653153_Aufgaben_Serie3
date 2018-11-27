0c2dc871065f2d41aad9279ab7688ee7eae1e57b
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-27 13:30:04 +0100

    Aufgabe 1

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,36 @@
+import javax.swing.*;
+
 public class Main
 {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
+
+        String eingabe = JOptionPane.showInputDialog("Bitte eingeben:");
+
+        switch(eingabe)
+        {
+            case "20":
+
+                int a = Integer.parseInt(eingabe);
+                int ergebnis = a * 26;
+
+                JOptionPane.showMessageDialog(null, "Das Ergebnis beträgt: " + ergebnis);
+
+                break;
+
+            case "Peter Pan":
+
+                JOptionPane.showMessageDialog(null, "Bangarang");
+
+                break;
+
+            default:
+
+                System.err.println("Keine Eingabe!");
+
+        }
+
     }
 
 }
