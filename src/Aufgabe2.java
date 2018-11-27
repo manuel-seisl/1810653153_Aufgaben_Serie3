f17db7fb47b8c99ef015618446297947f1ee82ac
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-27 13:41:35 +0100

    Aufgabe 3

--- src/Aufgabe2.java
+++ src/Aufgabe2.java
@@ -4,8 +4,9 @@ public class Aufgabe2
     public static void main(String[] args)
     {
 
-        for(int i = 0; i < 5; i += 2){
+        for(int i = 0; i < 5; i++){
             printName("Manuel Seisl");
+            printAge(26 + i);
         }
 
     }
@@ -13,6 +14,11 @@ public class Aufgabe2
     public static void printName(String name)
     {
         System.out.println(name);
+    }
+
+    public static void printAge(int age)
+    {
+        System.out.println(age);
     }
 
 }
