2049e49354dca060c1d8b9ae8bfa9f8bb796fd59
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-27 13:38:19 +0100

    Aufgabe 2

---
+++ src/Aufgabe2.java
@@ -0,0 +1,18 @@
+public class Aufgabe2
+{
+
+    public static void main(String[] args)
+    {
+
+        for(int i = 0; i < 5; i += 2){
+            printName("Manuel Seisl");
+        }
+
+    }
+
+    public static void printName(String name)
+    {
+        System.out.println(name);
+    }
+
+}
