57cd1f90c5d2a351850ce2e3aa65b1a8df94d58a
manuel-seisl <1810653153@stud.fh-kufstein.ac.at>
2018-11-27 13:49:24 +0100

    Aufgabe 5

---
+++ src/Rechnen.java
@@ -0,0 +1,20 @@
+public class Rechnen
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println(add(2, 3));
+        System.out.println(add(1.5, 2.3));
+    }
+
+    public static int add(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static double add(double a, double b)
+    {
+        return a + b;
+    }
+
+}
